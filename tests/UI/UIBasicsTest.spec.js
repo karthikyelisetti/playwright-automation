632fa0bf88f53c4ec3aa8e6a92b763816dcf4aa8
karthik.yelisetti <karthikyelisetti.dev@gmail.com>
2026-05-27 18:07:09 +0530

    Extracting multiple web elements from page

--- tests/UI/UIBasicsTest.spec.js
+++ tests/UI/UIBasicsTest.spec.js
@@ -3,16 +3,35 @@ const {test, expect} = require('@playwright/test');
 test.only('Browser context UI Demo', async ({browser}) => {
     const context = await browser.newContext();
     const page = await context.newPage();
+
+    // locators for the elements on the page
+    const username = page.locator("#username");
+    const password = page.locator("[type='password']");
+    const signInBtn = page.locator("#signInBtn");
+    const alertMsg = page.locator("[style*='block']");
+
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log(await page.title());
 
-    await page.locator("#username").fill("rahulshettyacademy");
-    await page.locator("[type='password']").fill("123456");
-    await page.locator("#signInBtn").click();
+    await username.fill("rahulshettyacademy");
+    await password.fill("123456");
+    await signInBtn.click();
 
-    console.log(await page.locator("[style*='block']").textContent());
+    // extract text from the alert and print it
+    console.log(await alertMsg.textContent());
 
-    await expect(page.locator("[style*='block']")).toContainText("Incorrect");
+    // assertion to check if the alert contains the expected text
+    await expect(alertMsg).toContainText("Incorrect");
+
+    await password.fill("");
+    await password.fill("Learning@830$3mK2");
+    await signInBtn.click();
+
+    // extract text from the page and cards title and print it
+    console.log(await page.locator(".card-body a").first().textContent());
+    console.log(await page.locator(".card-body a").nth(1).textContent());
+    console.log(await page.title());
+
 });
 
 test('Page UI Demo', async ({page}) => {
