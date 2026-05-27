c1eb55e24a33c93433ce7692cc2d1783da783b9f
karthik.yelisetti <karthikyelisetti.dev@gmail.com>
2026-05-27 14:23:51 +0530

    setting the browser headless to false

--- playwright.config.js
+++ playwright.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     browserName: 'chromium',
+    headless: false, // Set headless to false to see the browser action
   }
 });
 
