1d10cdd59d1fc4e0a4c3005c1c9a2007ae79f424
karthik.yelisetti <karthikyelisetti.dev@gmail.com>
2026-05-27 13:59:23 +0530

    Playwright UIBasicTest with Browser and Page implementation

---
+++ playwright.config.js
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './tests',
+  timeout: 40 * 1000,
+  expect: {
+    timeout: 40 * 1000
+  },
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    browserName: 'chromium',
+  }
+});
+
---
+++ tests/UI/UIBasicsTest.spec.js
@@ -0,0 +1,16 @@
+const {test, expect} = require('@playwright/test');
+
+test('Browser context UI Demo', async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    console.log(await page.title());
+});
+
+test('Page UI Demo', async ({page}) => {
+    await page.goto("https://google.com/");
+    console.log(await page.title());
+
+    await expect(page).toHaveTitle("Google");
+});
