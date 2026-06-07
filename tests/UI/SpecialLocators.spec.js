94f6de993d2c92461524452d10ecf7dadde5ded1
karthik.yelisetti <karthikyelisetti.dev@gmail.com>
2026-06-07 17:45:35 +0530

    Playwright Special Locators

---
+++ tests/UI/SpecialLocators.spec.js
@@ -0,0 +1,21 @@
+const {test} = require('@playwright/test');
+
+
+test.only("Playwright Special Locators", async ({page}) => {
+    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+
+    await page.getByLabel("Check me out if you Love IceCreams!").click();
+    await page.getByLabel("Employed").click();
+    await page.getByLabel("Gender").selectOption("Female");
+
+    await page.locator(".form-group input[name='name']").fill("Demo Test");
+    await page.getByPlaceholder("Password").fill("123456");
+
+    await page.getByRole("button", {name: "Submit"}).click();
+
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    await page.getByRole("Link", {name: "Shop"}).click();
+    await page.locator("app-card").filter({hasText: "Nokia Edge"}).getByRole("button").click();
+
+    await page.pause();
+});
